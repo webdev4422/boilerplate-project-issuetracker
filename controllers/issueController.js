7c68f0718ee6441d1f11a7f1b5ec4ae42950d0d4
user <user@MacBook-Pro-user.local>
2023-08-17 12:01:33 +0300

    Refactor

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -3,44 +3,47 @@ const ObjectId = mongoose.Types.ObjectId
 
 const { IssueModel, ProjectModel } = require('../models/issueModel.js')
 
-// *** POST ISSUE***
-// POST /api/issues/apitest?issue_title=titleX&issue_text=textX&created_by=userX
+// *** POST ISSUE ***
+// POST /api/issues/apitest Form Encoded: issue_title=titleX&issue_text=textX&created_by=userX
 const postIssue = async (req, res) => {
   let projectName = req.params.project // this take value from /api/issues/ANYPROJECT
-  // let bodyX = req.body
-  // const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = bodyX
+
+  // Use destructuring assignment to pull all fields from body object
+  let reqBody = req.body
+  const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = reqBody
+
+  // Use try...catch block to handle errors for async operations
   try {
     // Check required fields
-    if (req.body.issue_title && req.body.issue_text && req.body.created_by) {
-      // Create issue
-      // CUATION ! use either save() or create(), but not both, because it's duplicate https://stackoverflow.com/questions/38290684/mongoose-save-vs-insert-vs-create
-      // const issueX = await IssueModel.create({
-      const issueX = new IssueModel({
-        // _id automatically added
-        issue_title: req.body.issue_title, // Required
-        issue_text: req.body.issue_text, // Required
-        created_by: req.body.created_by, // Required
-        assigned_to: req.body.assigned_to, // Return empty
-        status_text: req.body.status_text, // Return empty
-        open: true, // Default true
-        created_on: new Date(), // Required
-        updated_on: new Date(), // Required
-      })
-
-      // Find project with ASYNC opearation, must use 'await'!
-      let projectX = await ProjectModel.findOne({ project_name: projectName })
-      // If project doesn't exists, create one
-      if (!projectX) {
-        projectX = await ProjectModel.create({ project_name: projectName })
-      }
-      // If project exists push issue into this project
-      projectX.issues.push(issueX)
-      // Save to database
-      await projectX.save()
-      return res.json(issueX)
-    } else {
+    if (!issue_title || !issue_text || !created_by)
       return res.json({ error: 'required field(s) missing' })
-    }
+
+    // Create issue
+    // CUATION ! use either save() or create(), but not both, because it's duplicate https://stackoverflow.com/questions/38290684/mongoose-save-vs-insert-vs-create
+    const issueX = new IssueModel({
+      // _id automatically added
+      issue_title: issue_title, // Required
+      issue_text: issue_text, // Required
+      created_by: created_by, // Required
+      assigned_to: assigned_to, // Return empty
+      status_text: status_text, // Return empty
+      open: true, // Default true
+      created_on: new Date(), // Required
+      updated_on: new Date(), // Required
+    })
+
+    // Find project with ASYNC opearation, must use 'await'!
+    let projectX = await ProjectModel.findOne({ project_name: projectName })
+
+    // If project doesn't exists, create one
+    if (!projectX) projectX = await ProjectModel.create({ project_name: projectName })
+
+    // If project exists push issue into this project
+    projectX.issues.push(issueX)
+
+    // Save to database
+    await projectX.save()
+    return res.json(issueX)
   } catch (error) {
     console.log(error)
     return res.status(500).json({ error: 'Server error' })
@@ -48,7 +51,7 @@ const postIssue = async (req, res) => {
 }
 
 // *** GET ISSUE ***
-// GET /api/issues/apitest
+// GET /api/issues/apitest?open=false&issue_title=titleX&issue_text=textX
 const getIssue = async (req, res) => {
   let projectName = req.params.project
   let reqQuery = req.query
@@ -76,12 +79,11 @@ const getIssue = async (req, res) => {
 }
 
 // *** PUT ISSUE ***
-// PUT /api/issues/apitest?issue_title=titleX&issue_text=textX&created_by=userX
+// PUT /api/issues/apitest Form Encoded: issue_title=titleX&issue_text=textX&created_by=userX
 const putIssue = async (req, res) => {
   let projectName = req.params.project
-  const queryX = req.body
-  // prettier-ignore Use destructuring assignment
-  const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = queryX
+  const reqBody = req.body
+  const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = reqBody
 
   try {
     if (!_id) return res.json({ error: 'missing _id' })
@@ -118,7 +120,7 @@ const putIssue = async (req, res) => {
       { new: true }
     )
     // Log updated issue using find functionality
-    console.log(findOneAndUpdate.issues.find((issue) => issue['_id'].toString() === _id))
+    // console.log(findOneAndUpdate.issues.find((issue) => issue['_id'].toString() === _id))
 
     if (!findOneAndUpdate) return res.json({ error: 'could not update', _id: _id })
 
@@ -129,7 +131,8 @@ const putIssue = async (req, res) => {
   }
 }
 
-// delete: /api/thread/:board thread_id=6458d90a153be09f10013a53; delete_password=xxx
+// *** DELETE ISSUE ***
+// DELETE /api/issues/apitest Form Encoded: _id=64ddeeeb5977467f332b2f7a
 const deleteIssue = async (req, res) => {
   let projectName = req.params.project
   const { _id } = req.body
@@ -138,16 +141,12 @@ const deleteIssue = async (req, res) => {
     if (!req.body._id) return res.json({ error: 'missing _id' })
     if (!ObjectId.isValid(_id)) return res.json({ error: 'could not delete', _id: _id })
 
-    // const projectX = await ProjectModel.findOne({ project_name: projectName })
-    // const issueX = projectX.issues.find((issue) => issue['_id'].toString() === _id)
-    // if (!issueX) return res.json({ error: 'could not delete', _id: _id })
-
-    // Delete (update with $pull)
+    // Delete (update with $pull) https://dev.to/paulasantamaria/mongodb-animated-adding-and-removing-elements-from-arrays-50cl
     const confirmDelete = await ProjectModel.updateOne(
       { 'issues._id': _id },
       { $pull: { issues: { _id: _id } } }
     )
-    console.log(confirmDelete)
+    // console.log(confirmDelete.matchedCount)
     if (!confirmDelete.matchedCount) return res.json({ error: 'could not delete', _id: _id })
 
     return res.json({ result: 'successfully deleted', _id: _id })
